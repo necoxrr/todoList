b341d5142f85e4bde824eb92f22e19cfdeb9b39a
Search Lr <i675love17@hotmail.com>
2023-10-30 21:26:25 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 const newTodo = ref('')
 const todos = ref([])
 const finishTodo = ref([])
+const visible = false
 
 // 添加新的事项的事件
 let next = 4
@@ -25,6 +26,19 @@ function deleteTodo(todo) {
 
 }
 
+// 编辑代办事项
+// 这一处有一个问题--就是直接在函数里改变visible的值是不可以的，现在所可以想的是直接用watch来监听visble
+function edittodo() {
+
+
+
+
+
+
+  // 弹出修改框
+
+}
+
 </script>
 <template>
   <div class="todo">
@@ -37,7 +51,7 @@ function deleteTodo(todo) {
       <ul class="todolist">
         <!-- 为什么在li中加（index,todo） in todos,{{ todo.titie }}中的文字显示不出来？？？ -->
         <li v-for="   todo in todos" :key="todo.id"><input type="checkbox" @click="deleteTodo(todo)"
-            style="width: 40px;height: 20px;" /><span>{{ todo.title }}</span>
+            style="width: 40px;height: 20px;" /><span>{{ todo.title }}</span><button @click="edittodo()">编辑</button>
         </li>
       </ul>
     </div>
@@ -47,12 +61,18 @@ function deleteTodo(todo) {
       <span
         style="width: 50px;height: 20px; background-color: cadetblue;font-family:'Times New Roman', Times, serif;">已完成</span>
       <ul class="todolist">
-        <li v-for="finishdo in finishTodo" :key="finishdo.id"><span>{{ finishdo.title1 }}</span></li>
+        <li v-for="finishdo in finishTodo" :key="finishdo.id" class="finishid"><span>{{ finishdo.title1 }}</span></li>
       </ul>
+    </div>
+
+    <!-- 弹出的修改事项框 -->
+    <div style="width: 100px;height: 100px;background-color: bisque;" v-show="visible == true">
+
     </div>
   </div>
 </template>
 <style>
+/* 去标签黑点的样式 */
 .todolist {
   list-style-type: none;
 }
@@ -69,5 +89,10 @@ function deleteTodo(todo) {
 /* 完成板块的显示 */
 .finish {
   background-color: aquamarine;
+}
+
+/* 完成事项的设计 */
+.finishid {
+  background-color: gray;
 }
 </style>
